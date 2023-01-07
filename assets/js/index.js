5cffa96bc195beadaff3cc5883467e41b3ddf92c
laysa <webdevlay@gmail.com>
2023-01-07 00:31:51 -0300

    últimas atualizações do envio do formulário via PHP e pagina de agradecimento pelo contato/redirecionamento para o site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
 function scrollTo (element) {
     document.querySelector(element).scrollIntoView({behavior: 'smooth'});
 }
-
+//scroll do botão up
 document.querySelector('#topo').addEventListener('click', function (event) {
     event.preventDefault();
     scrollTo('.logotipo');
 });
-
+//scroll do menu #sobre
 document.querySelector('#about').addEventListener('click', function (event) {
     event.preventDefault();
     scrollTo('#second-left');
 });
-
+//scroll do menu #habilidades
 document.querySelector('#skills').addEventListener('click', function (event) {
     event.preventDefault();
     scrollTo('#second-right');
 });
-
+//scroll do menu #portfolio
 document.querySelector('#portfolio').addEventListener('click', function (event) {
     event.preventDefault();
     scrollTo('#third-level');
 });
-
+//scroll do menu #contato
 document.querySelector('#contact').addEventListener('click', function (event) {
     event.preventDefault();
     scrollTo('#fourth-level');
