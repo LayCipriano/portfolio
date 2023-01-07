509bfe834cc712c3c9ad4cde8df2b11b300474c3
laysa <webdevlay@gmail.com>
2023-01-07 00:38:19 -0300

    últimas atualizações do envio do formulário via PHP e pagina de agradecimento pelo contato/redirecionamento para o site

---
+++ assets/php/formulario.php
@@ -0,0 +1,35 @@
+<?php
+    if(isset($_POST['btn-contato'])) {
+
+    $nome = $_POST['name'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['message'];
+
+
+    //Corpo email
+    $arquivo = "<html>
+        <p><b>Nome: </b>$nome</p>
+        <p><b>E-mail: </b>$email</p>
+        <p><b>Mensagem: </b>$mensagem</p>
+        </html>";
+    
+        //destino dos e-mail
+    $destino = "contato@layaltheman.com";
+    $assunto = "Contato via www.layaltheman.com";
+
+    //formatação padrao para e-mail PHP
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+    $headers .= "From: $nome <$email>";
+
+    //Enviar
+    mail($destino, $assunto, $arquivo, $headers);
+    
+    header("Location: ../pages/thankYou.html");
+    // exit();
+    // } else {
+    //     include_once('../falhaCarregamento.html');
+    //     echo "<meta http-equiv='refresh' content='5;URL=../index.html'>";
+
+    }
+?>
