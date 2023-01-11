7f1c30c6d9a8ff67bea77070a57dafa4ebd7afe1
laysa <webdevlay@gmail.com>
2023-01-11 01:16:12 -0300

    adicionado layout responsivo via CSS + atualizações no JavaScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,35 @@
-function scrollTo (element) {
-    document.querySelector(element).scrollIntoView({behavior: 'smooth'});
-}
-//scroll do botão up
-document.querySelector('#topo').addEventListener('click', function (event) {
-    event.preventDefault();
-    scrollTo('.logotipo');
-});
-//scroll do menu #sobre
-document.querySelector('#about').addEventListener('click', function (event) {
-    event.preventDefault();
-    scrollTo('#second-left');
-});
-//scroll do menu #habilidades
-document.querySelector('#skills').addEventListener('click', function (event) {
-    event.preventDefault();
-    scrollTo('#second-right');
-});
-//scroll do menu #portfolio
-document.querySelector('#portfolio').addEventListener('click', function (event) {
-    event.preventDefault();
-    scrollTo('#third-level');
-});
-//scroll do menu #contato
-document.querySelector('#contact').addEventListener('click', function (event) {
-    event.preventDefault();
-    scrollTo('#fourth-level');
-});
+    function scrollTo (element) {
+        document.querySelector(element).scrollIntoView({behavior: 'smooth'});
+    }
+    //scroll do botão up
+    document.querySelector('#topo').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollTo('.logotipo');
+    });
+    //scroll do menu #sobre
+    document.querySelector('#about').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollTo('#second-left');
+    });
+    //scroll do menu #habilidades
+    document.querySelector('#skills').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollTo('#second-right');
+    });
+    //scroll do menu #portfolio
+    document.querySelector('#portfolio').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollTo('#third-level');
+    });
+    //scroll do menu #contato
+    document.querySelector('#contact').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollTo('#fourth-level');
+    });
+
+    const menu = document.querySelector('#menu');
+    var close = document.querySelector('.links-navega');
+        menu.addEventListener('click', () => {
+            close.classList.toggle('show');
+        });
+    
